1c24daacf18b630aac38ff93c251cbe41ee5870f
Hiroaki Nakamura <hnakamur@gmail.com>
2016-03-25 19:53:04 +0900

    Create host group, host, item and send data before getting history data

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
+	"net"
 	"os"
 	"time"
 
+	"github.com/AlekSi/zabbix-sender"
 	"github.com/hnakamur/go-zabbix"
 )
 
@@ -18,7 +22,95 @@ func getHostCount(client *zabbix.Client) (int64, error) {
 	return client.CallForCount("host.get", params)
 }
 
-func getHostID(client *zabbix.Client, hostname string) (string, error) {
+const zabbixItemValueTypeNumericUnsigned = 3
+
+func createHostGroupIfNotExists(client *zabbix.Client, hostGroupName string) (hostGroupID string, err error) {
+	hostGroupID, err = getHostGroupID(client, hostGroupName)
+	if err != nil {
+		if err != errNotFound {
+			return
+		}
+		hostGroupID, err = createHostGroup(client, hostGroupName)
+	}
+	return
+}
+
+func createHostIfNotExists(client *zabbix.Client, hostGroupID, hostname string) (hostID string, err error) {
+	hostID, err = getHostID(client, hostGroupID, hostname)
+	if err != nil {
+		if err != errNotFound {
+			return
+		}
+		hostID, err = createHost(client, hostGroupID, hostname)
+	}
+	return
+}
+
+func createIntegerItemIfNotExists(client *zabbix.Client, hostID, itemKey, itemName string) (itemID string, err error) {
+	itemID, err = getItemID(client, hostID, itemKey)
+	if err != nil {
+		if err != errNotFound {
+			return
+		}
+		itemID, err = createIntegerItem(client, hostID, itemKey, itemName)
+	}
+	return
+}
+
+var errNotFound = errors.New("not found")
+
+func getHostGroupID(client *zabbix.Client, hostGroupName string) (hostGroupID string, err error) {
+	type filter struct {
+		Name string `json:"name"`
+	}
+	params := struct {
+		Filter filter   `json:"filter"`
+		Output []string `json:"output"`
+	}{
+		Filter: filter{
+			Name: hostGroupName,
+		},
+		Output: []string{"groupid"},
+	}
+
+	var groups []struct {
+		GroupID string `json:"groupid"`
+	}
+	err = client.Call("hostgroup.get", params, &groups)
+	if err != nil {
+		return
+	}
+	if len(groups) == 0 {
+		err = errNotFound
+		return
+	}
+	hostGroupID = groups[0].GroupID
+	return
+}
+
+func createHostGroup(client *zabbix.Client, hostGroupName string) (hostGroupID string, err error) {
+	params := struct {
+		Name string `json:"name"`
+	}{
+		Name: hostGroupName,
+	}
+
+	var result struct {
+		GroupIDs []string `json:"groupids"`
+	}
+	err = client.Call("hostgroup.create", params, &result)
+	if err != nil {
+		return
+	}
+	if len(result.GroupIDs) == 0 {
+		err = errNotFound
+		return
+	}
+	hostGroupID = result.GroupIDs[0]
+	return
+}
+
+func getHostID(client *zabbix.Client, hostGroupID, hostname string) (hostID string, err error) {
 	type filter struct {
 		Host string `json:"host"`
 	}
@@ -35,24 +127,76 @@ func getHostID(client *zabbix.Client, hostname string) (string, error) {
 	var hosts []struct {
 		HostID string `json:"hostid"`
 	}
-	err := client.Call("host.get", params, &hosts)
+	err = client.Call("host.get", params, &hosts)
 	if err != nil {
-		return "", err
+		return
 	}
-	if len(hosts) != 1 {
-		return "", fmt.Errorf("expected 1 host but got %d", len(hosts))
+	if len(hosts) == 0 {
+		err = errNotFound
+		return
 	}
-	return hosts[0].HostID, nil
+	hostID = hosts[0].HostID
+	return
 }
 
-func getItemID(client *zabbix.Client, itemKey string) (string, error) {
+func createHost(client *zabbix.Client, hostGroupID, hostname string) (hostID string, err error) {
+	type hostInterface struct {
+		Type  int    `json:"type"`
+		Main  int    `json:"main"`
+		UseIP int    `json:"useip"`
+		IP    string `json:"ip"`
+		Port  string `json:"port"`
+		DNS   string `json:"dns"`
+	}
+	type group struct {
+		GroupID string `json:"groupid"`
+	}
+	params := struct {
+		Host       string          `json:"host"`
+		Interfaces []hostInterface `json:"interfaces"`
+		Groups     []group         `json:"groups"`
+	}{
+		Host: hostname,
+		Interfaces: []hostInterface{
+			{
+				Type:  1, // Agent
+				Main:  1,
+				UseIP: 1,
+				IP:    "127.0.0.1",
+				Port:  "10050",
+				DNS:   "",
+			},
+		},
+		Groups: []group{
+			{GroupID: hostGroupID},
+		},
+	}
+
+	var result struct {
+		HostIDs []string `json:"hostids"`
+	}
+	err = client.Call("host.create", params, &result)
+	if err != nil {
+		return
+	}
+	if len(result.HostIDs) == 0 {
+		err = errNotFound
+		return
+	}
+	hostID = result.HostIDs[0]
+	return
+}
+
+func getItemID(client *zabbix.Client, hostID, itemKey string) (itemID string, err error) {
 	type search struct {
 		Key string `json:"key_"`
 	}
 	params := struct {
-		Search search   `json:"search"`
-		Output []string `json:"output"`
+		HostIDs string   `json:"hostids"`
+		Search  search   `json:"search"`
+		Output  []string `json:"output"`
 	}{
+		HostIDs: hostID,
 		Search: search{
 			Key: itemKey,
 		},
@@ -61,14 +205,75 @@ func getItemID(client *zabbix.Client, itemKey string) (string, error) {
 	var items []struct {
 		ItemID string `json:"itemid"`
 	}
-	err := client.Call("item.get", params, &items)
+	err = client.Call("item.get", params, &items)
 	if err != nil {
-		return "", err
+		return
 	}
-	if len(items) != 1 {
-		return "", fmt.Errorf("expected 1 item but got %d", len(items))
+	if len(items) == 0 {
+		err = errNotFound
+		return
 	}
-	return items[0].ItemID, nil
+	itemID = items[0].ItemID
+	return
+}
+
+func createIntegerItem(client *zabbix.Client, hostID, itemKey, itemName string) (itemID string, err error) {
+	params := struct {
+		Name      string `json:"name"`
+		Key       string `json:"key_"`
+		HostID    string `json:"hostid"`
+		Type      int    `json:"type"`
+		ValueType int    `json:"value_type"`
+		Status    int    `json:"status"`
+	}{
+		Name:      itemName,
+		Key:       itemKey,
+		HostID:    hostID,
+		Type:      2, // Zabbix trapper
+		ValueType: zabbixItemValueTypeNumericUnsigned,
+		Status:    0, // enabled
+	}
+
+	var result struct {
+		ItemIDs []string `json:"itemids"`
+	}
+	err = client.Call("item.create", params, &result)
+	if err != nil {
+		return
+	}
+	if len(result.ItemIDs) == 0 {
+		err = errNotFound
+		return
+	}
+	itemID = result.ItemIDs[0]
+	return
+}
+
+func getHistories(client *zabbix.Client, params, histories interface{}) error {
+	return client.Call("history.get", params, &histories)
+}
+
+func sendItemData(zabbixServerAddr, hostname string, data map[string]interface{}, t time.Time, logger zabbix.Logger) error {
+	items := zabbix_sender.MakeDataItems(data, hostname)
+	for i, _ := range items {
+		items[i].Timestamp = t.Unix()
+	}
+	addr, err := net.ResolveTCPAddr("tcp", zabbixServerAddr)
+	if err != nil {
+		return err
+	}
+	res, err := zabbix_sender.Send(addr, items)
+	if err != nil {
+		return err
+	}
+	if logger != nil {
+		buf, err := json.Marshal(data)
+		if err != nil {
+			logger.Log(err.Error())
+		}
+		logger.Log(fmt.Sprintf("sent data. timeout, time=%s data=%s, res=%s", t.Format(time.RFC3339), string(buf), *res))
+	}
+	return nil
 }
 
 type myLogger struct {
@@ -88,33 +293,68 @@ func main() {
 		logger.Fatal(err)
 	}
 
+	hostGroupID, err := createHostGroupIfNotExists(client, "Go-Zabbix example group")
+	if err != nil {
+		logger.Fatal(err)
+	}
+	hostname := "web1.example.com"
+	hostID, err := createHostIfNotExists(client, hostGroupID, hostname)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	itemKey := "access_count"
+	itemName := "Access Count"
+	itemID, err := createIntegerItemIfNotExists(client, hostID, itemKey, itemName)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
 	count, err := getHostCount(client)
 	if err != nil {
 		logger.Fatal(err)
 	}
 	logger.Printf("host count=%d", count)
 
-	hostID, err := getHostID(client, "xxxxx.example.com")
+	zabbixServerAddr := "localhost:10051"
+
+	data := map[string]interface{}{itemKey: 0}
+	t := time.Now()
+	err = sendItemData(zabbixServerAddr, hostname, data, t, logger)
 	if err != nil {
 		logger.Fatal(err)
 	}
+	time.Sleep(time.Duration(10) * time.Second)
 
-	itemID, err := getItemID(client, "body_bytes_sent")
+	data = map[string]interface{}{itemKey: 10}
+	t = time.Now()
+	err = sendItemData(zabbixServerAddr, hostname, data, t, logger)
 	if err != nil {
 		logger.Fatal(err)
 	}
+	time.Sleep(time.Duration(10) * time.Second)
 
-	params := map[string]interface{}{
-		"history":   3,
-		"hostids":   hostID,
-		"itemids":   itemID,
-		"time_from": zabbix.Timestamp(time.Date(2016, 3, 23, 22, 33, 0, 0, time.Local)),
+	data = map[string]interface{}{itemKey: 0}
+	t = time.Now()
+	err = sendItemData(zabbixServerAddr, hostname, data, t, logger)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	time.Sleep(time.Duration(10) * time.Second)
+
+	params := struct {
+		History int    `json:"history"`
+		HostIDs string `json:"hostids"`
+		ItemIDs string `json:"itemids"`
+	}{
+		History: zabbixItemValueTypeNumericUnsigned,
+		HostIDs: hostID,
+		ItemIDs: itemID,
 	}
 	var histories []struct {
 		Clock zabbix.Timestamp `json:"clock,string"`
 		Value uint64           `json:"value,string"`
 	}
-	err = client.Call("history.get", params, &histories)
+	err = getHistories(client, params, &histories)
 	if err != nil {
 		logger.Fatal(err)
 	}

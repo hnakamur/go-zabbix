26eccfe8674edca30ba69e911a3121d021328084
Hiroaki Nakamura <hnakamur@gmail.com>
2016-04-23 18:37:50 +0900

    Fix a typo

--- doc.go
+++ doc.go
@@ -3,6 +3,6 @@
 // This library is provides the only minimal lower level APIs.
 //
 // Users can refer to https://www.zabbix.com/documentation/3.0/manual/api
-// and create functions for their use cases like an exmaple in this package.
+// and create functions for their use cases like an example in this package.
 // https://github.com/hnakamur/go-zabbix/blob/master/cmd/example/main.go
 package zabbix

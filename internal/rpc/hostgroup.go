bc8c08f8b99d2bf48a4a7c9e485d8c046a65640f
Hiroaki Nakamura <hnakamur@gmail.com>
2024-10-18 14:27:16 +0900

    Add --include-nested flag to expand nested host groups
    Also add --dry-run global flag.

--- internal/rpc/hostgroup.go
+++ internal/rpc/hostgroup.go
@@ -47,3 +47,42 @@ func (c *Client) GetHostGroupsByNamesFullMatch(ctx context.Context,
 	}
 	return groups, nil
 }
+
+func (c *Client) GetNestedHostGroupsByAncestorNames(ctx context.Context,
+	names []string) ([]HostGroup, error) {
+
+	params := struct {
+		Output any `json:"output"`
+	}{
+		Output: []string{"groupid", "name"},
+	}
+	var groups []HostGroup
+	if err := c.Client.Call(ctx, "hostgroup.get", params, &groups); err != nil {
+		return nil, err
+	}
+
+	var notFoundNames []string
+	for _, name := range names {
+		if !slices.ContainsFunc(groups, func(grp HostGroup) bool {
+			return grp.Name == name
+		}) {
+			notFoundNames = append(notFoundNames, name)
+		}
+	}
+	if len(notFoundNames) > 0 {
+		return nil, fmt.Errorf("host groups not found: %s", strings.Join(notFoundNames, ", "))
+	}
+	return filterHostGroupsByAncestorNames(groups, names), nil
+}
+
+func filterHostGroupsByAncestorNames(groups []HostGroup, names []string) []HostGroup {
+	var filteredGroups []HostGroup
+	for _, group := range groups {
+		for _, name := range names {
+			if group.Name == name || strings.HasPrefix(group.Name, name+"/") {
+				filteredGroups = append(filteredGroups, group)
+			}
+		}
+	}
+	return filteredGroups
+}
